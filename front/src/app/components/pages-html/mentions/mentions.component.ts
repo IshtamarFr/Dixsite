c363034e1087c0d964c0122b844b536cd774023f
IshtamarFr <ishta@ishtamar.fr>
2024-04-18 15:13:50 +0200

    legal notice in english is integrated

--- front/src/app/components/pages-html/mentions/mentions.component.ts
+++ front/src/app/components/pages-html/mentions/mentions.component.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { AppSettings } from '../../../utils/app-settings';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-mentions',
@@ -11,8 +12,15 @@ import { AppSettings } from '../../../utils/app-settings';
   templateUrl: './mentions.component.html',
   styleUrl: './mentions.component.scss',
 })
-export class MentionsComponent {
+export class MentionsComponent implements OnInit {
   CONTACT_EMAIL = AppSettings.CONTACT_EMAIL;
+  public english: boolean = false;
+
+  constructor(private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.english = this.activatedRoute.snapshot.url.toString().includes('/en/');
+  }
 
   public back() {
     window.history.back();
